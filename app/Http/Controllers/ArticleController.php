e8375c520b981a94d42e2dbbf75cbabb61cbeddf
dhane31 <dhanegm731@gmail.com>
2021-02-19 14:00:19 +0900

    Update tags in create, edit view

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Tag;
 use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
@@ -29,17 +30,36 @@ class ArticleController extends Controller
     $article->fill($request->all());
     $article->user_id = $request->user()->id;
     $article->save();
+
+    $request->tags->each(function ($tagName) use ($article) {
+      $tag = Tag::firstOrCreate(['name' => $tagName]);
+      $article->tags()->attach($tag);
+    });
+
     return redirect()->route('articles.index');
   }
 
   public function edit(Article $article)
   {
-    return view('articles.edit', ['article' => $article]);
+    $tagNames = $article->tags->map(function ($tag) {
+      return ['text' => $tag->name];
+    });
+    return view('articles.edit', [
+      'article' => $article,
+      'tagNames' => $tagNames,
+    ]);
   }
 
   public function update(ArticleRequest $request, Article $article)
   {
     $article->fill($request->all())->save();
+
+    $article->tags()->detach();
+    $request->tags->each(function ($tagName) use ($article) {
+      $tag = Tag::firstOrCreate(['name' => $tagName]);
+      $article->tags()->attach($tag);
+    });
+
     return redirect()->route('articles.index');
   }
 
