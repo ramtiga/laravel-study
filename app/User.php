08a2c4fbb19849d43423d20f3dc43837c4c16a1f
dhane31 <dhanegm731@gmail.com>
2021-02-22 15:28:33 +0900

    Add articles, likes in my page.

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use App\Mail\BareMail;
 use App\Notifications\PasswordResetNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -58,6 +59,16 @@ class User extends Authenticatable
   public function followings(): BelongsToMany
   {
     return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
+  }
+
+  public function articles(): HasMany
+  {
+    return $this->hasMany('App\Article');
+  }
+
+  public function likes(): BelongsToMany
+  {
+    return $this->belongsToMany('App\Article', 'likes')->withTimestamps();
   }
 
   public function getCountFollowersAttribute(): int
