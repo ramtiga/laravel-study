4a8121c0a27bf92521c3b8e678a0a9109454ff89
dhane31 <dhanegm731@gmail.com>
2021-02-22 09:48:59 +0900

    Add follows

--- app/User.php
+++ app/User.php
@@ -5,42 +5,53 @@ namespace App;
 use App\Mail\BareMail;
 use App\Notifications\PasswordResetNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    public function sendPasswordResetNotification($token)
-    {
-        $this->notify(new PasswordResetNotification($token, new BareMail()));
-    }
+  public function sendPasswordResetNotification($token)
+  {
+    $this->notify(new PasswordResetNotification($token, new BareMail()));
+  }
+
+  public function followers(): BelongsToMany
+  {
+    return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimeStamps();
+  }
+
+  public function isFollowedBy(?User $user): bool
+  {
+    return $user ? (bool)$this->followers->where('id', $user->id)->count() : false;
+  }
 }
